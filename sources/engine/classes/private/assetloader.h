e5175aebbf9b6ef5b3dcdd455ac7c29c55cfe586
Patrick <patrick.lie9@gmail.com>
2022-01-08 00:32:33 +0100

    adding array stuff

---
+++ sources/engine/classes/private/assetloader.h
@@ -0,0 +1,8 @@
+
+
+class AssetLoader {
+    private:
+        
+    public:
+        
+};
--- sources/engine/classes/public/engine.cpp
+++ sources/engine/classes/public/engine.cpp
@@ -1,4 +1,6 @@
 #include "engine.h"
+#include <vector>
+#include <iostream>
 // Project
 Texture2D texture;
 /* saving snippet for later, for creating callbacks
@@ -16,10 +18,20 @@ execution: //ja(fun);
 
 /* ------------------------------------------------- Engine Calls ------------------------------------------------------------------- */
 
+std::vector<int> arr(0);
+
 void Engine::Start() {
     InitWindow(windowWidth, windowHeight, "TestGame mon");
     SetTargetFPS(120);
     texture = LoadTexture("../sources/resources/raylib_logo.png");
+    arr.push_back(1);
+    arr.push_back(1);
+    arr.push_back(1);
+
+    for (size_t i = 0; i < arr.size(); i++)
+    {
+        std::cout << arr[i] << std::endl;
+    }
 };
 
 void Engine::Update() {
@@ -27,7 +39,7 @@ void Engine::Update() {
 
 void Engine::Draw() {
     DrawFPS(windowWidth - 100, 50);
-    DrawTexture(texture, 100, 100, WHITE);
+    //DrawTexture(texture, 100, 100, WHITE);
 };
 
 int Engine::GetWinWidth() {
