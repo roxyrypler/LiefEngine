384fdb9dc056fe3e618b43d4ad91f0e47e68edfc
Patrick <patrick.lie9@gmail.com>
2022-01-08 19:12:38 +0100

    Update engine.cpp

--- sources/engine/classes/public/engine.cpp
+++ sources/engine/classes/public/engine.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 #include <iostream>
 // Project
-Texture2D texture;
+//Texture2D texture;
 /* saving snippet for later, for creating callbacks
 void fun() {
     std::cout << "Halla balla" << std::endl;
@@ -18,20 +18,11 @@ execution: //ja(fun);
 
 /* ------------------------------------------------- Engine Calls ------------------------------------------------------------------- */
 
-std::vector<std::string> arr(0);
 
 void Engine::Start() {
     InitWindow(windowWidth, windowHeight, "TestGame mon");
     SetTargetFPS(120);
-    texture = LoadTexture("../sources/resources/raylib_logo.png");
-    arr.push_back("1");
-    arr.push_back("2");
-    arr.push_back("3");
-
-    for (size_t i = 0; i < arr.size(); i++)
-    {
-        std::cout << arr[i] << std::endl;
-    }
+    //texture = LoadTexture("../sources/resources/raylib_logo.png");
 };
 
 void Engine::Update() {
